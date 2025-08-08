3d5871b21ac252f44577fc8089b99e4ba234402b
smash <2495592289@qq.com>
2025-08-08 19:26:59 +0800

    新增手机和美妆模板

---
+++ src/data/clothingPage/mockData.js
@@ -0,0 +1,51 @@
+export const mockData = {
+    product: {
+        id: 10020432707884,
+        title: "Beet & Berry",
+        handle: "2025-new-womens-underarm-bag-classic-printed-versatile-single-shoulder-for-commuting",
+        description:
+            "Cozy and sustainable clothing for all kiddos. Style your little one in fun clothing that is easy to wear, durable and timeless. This item has been thoughtfully made with high-quality materials. All products in this store are for demo purposes only. They have been generously provided by The Sunday Collective.",
+        published_at: "2025-06-23T08:49:41-04:00",
+        created_at: "2025-06-23T08:49:39-04:00",
+        vendor: "salerio-henry-test01",
+        type: "",
+        tags: [],
+        price: 11566,
+        price_min: 11566,
+        price_max: 12474,
+        available: true,
+        price_varies: true,
+        compare_at_price: null,
+        compare_at_price_min: 0,
+        compare_at_price_max: 0,
+        compare_at_price_varies: false,
+        images: [
+            "https://eight-nourish-demo.myshopify.com/cdn/shop/files/12_queenstown-big-front.jpg?v=1686843617&width=1445",
+            "https://eight-nourish-demo.myshopify.com/cdn/shop/files/12_queenstown-big-back.jpg?v=1686843617&width=1445",
+        ],
+        featured_image:
+            "https://eight-nourish-demo.myshopify.com/cdn/shop/files/12_queenstown-big-front.jpg?v=1686843617&width=1445",
+        options: ["Color"],
+        media: [],
+        requires_selling_plan: false,
+        selling_plan_groups: [],
+        selected_or_first_available_variant: {
+            id: 51271534706988,
+            title: "zong",
+            option1: "zong",
+            option2: null,
+            option3: null,
+        },
+        options_with_values: [
+            {
+                name: "Size",
+                position: 1,
+                values: ["500ml", "330ml", "250ml"],
+            },
+        ],
+    },
+    shop: {
+        currency: "USD",
+        money_format: "{{ amount }}",
+    },
+};
